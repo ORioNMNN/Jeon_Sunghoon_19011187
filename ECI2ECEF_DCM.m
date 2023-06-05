88fe7dfcd6c0129222e3145f9659fd1e0d854197
ORioNMNN <jsh02037@naver.com>
2023-06-06 02:24:17 +0900

    HW13

--- ECI2ECEF_DCM.m
+++ ECI2ECEF_DCM.m
@@ -6,7 +6,7 @@ function DCM = ECI2ECEF_DCM(time)
     
     theta_GMST = 280.46061837 + 360.98564736629 * (JD - 2451545.0) + 0.000387933 * T_UT1^2 - T_UT1^3/38710000; 
     theta_GMST = mod(theta_GMST, 360); 
-    theta_GMST = deg2rad(theta_GMST); % 
+    theta_GMST = deg2rad(theta_GMST); 
     
     DCM = [cos(theta_GMST) sin(theta_GMST) 0;
            -sin(theta_GMST) cos(theta_GMST) 0;
